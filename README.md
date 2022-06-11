879b92c23243456299b63c18e5a648248c670443
Nano <nanoc.contato@gmail.com>
2022-06-11 19:01:44 -0300

    Initial final changes, style updated and some logical reworked

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## A simple drink water app, to study LocalStorage and Notifications.
-Still in development, this is alpha.alpha.0.1 haha, and has no style at all 
 [Live version]()
--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-const waterInside = document.querySelector("[data-js=water-inside]")
-const percentText = document.querySelector("[data-js=water-inside-percent]")
-const smallCups = [...document.querySelector("[data-js=small-cups]").children]
+const waterInside = document.querySelector('[data-js="water-inside"]')
+const percentText = document.querySelector('[data-js="water-inside-percent"]')
+const percentActual = document.querySelector('[data-js="percent-Actual"]')
+const smallCups = [...document.querySelector('[data-js="small-cups"]').children]
 let waterPercent = 0
 let quantityWithSelected = 0
-
-//just to test
-let daysPassed = 0
 
 //Notification system
 const startNotification = () => {
      if(Notification.permission === "granted") {
         showNotification()
         timerToDrink()
+        //If the user didn't denied the permission, ask for
     } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(permission => {
             if(Notification.permission === "granted") {
@@ -20,29 +19,23 @@ const startNotification = () => {
             }
         })
     }
-
-    /*if(Notification.permission !== "denied") {
-        Notification.requestPermission().then(permission => {
-            if(Notification.permission === "granted") {
-                showNotification() 
-                timerToDrink()
-            }
-        })
-} */
 }
 
+//Notification body
 const showNotification = () => {
-    const notification = new Notification('hora de beber água!', {
-        body: "Essa notificação serve para a cada 1 hora, um aviso para tomar água seja emitido!",
+    const notification = new Notification('Bora beber água!', {
+        body: "Essa notificação serve para que a cada 1 hora, um aviso para tomar água seja emitido!",
         //icon:
     })
 }
 
+//Every one hour, call the notification again
 const timerToDrink = () => {
     const hourInMS = 60 * 60 * 1000
     const timerDrink = setInterval(()=>{
         showNotification()
 
+        //And stops the notification if the water is filled
         if(waterPercent === 100) {
             timerToReset()
             clearInterval(timerDrink)   
@@ -50,20 +43,21 @@ const timerToDrink = () => {
     }, hourInMS)
 }
 
+//If it reached 100%, set a timer to 6 hours, and reset the percent after
 const timerToReset = () => {
     const dayAfter = (60 * 60 * 1000) * 6
 
     const timerReset = setInterval(()=>{
         localStorage.setItem('waterPercent', 0)
         localStorage.setItem('quantitySelected', 0)
-        daysPassed += 1
-        localStorage.setItem('daysPassed', daysPassed)
 		startNotification()
         clearInterval(timerReset)
     }, dayAfter)
 }
 
+//To handle the local storage system
 const updateWithLocalStorage = () => {
+    //Loop and add the class based on the selected stored
     if(localStorage.quantitySelected > 1) {
         for(let index = 0; index < localStorage.quantitySelected; index++) {
             smallCups[index].classList.add('selected') 
@@ -71,13 +65,16 @@ const updateWithLocalStorage = () => {
         quantityWithSelected = Number(localStorage.quantitySelected)
     }
 
+    //Retrieve the percent, and update the visual
     if(localStorage.waterPercent > 0) { 
-        waterPercent = Number(localStorage.waterPercent)
+        //here I prevent a bug, setting the water percent to the amount of cups selecter * 12.5(percent)
+        waterPercent = Number(quantityWithSelected * 12.5)
         waterInside.style.height = `${localStorage.waterPercent}%`
         percentText.textContent = `${localStorage.waterPercent}%` 
     }
 }
 
+//Call when loaded
 document.addEventListener('DOMContentLoaded', ()=> {
     updateWithLocalStorage()
     startNotification()
@@ -89,20 +86,22 @@ const updateWater = event => {
 }
 
 const changeWaterInsideHeight = event => {
-    
     if(!event.target.classList.contains('selected')) {
         waterPercent -= 12.5
     } else {
         waterPercent += 12.5
     }
 
-    //atualiza certinho aqui, após o percent ser atualizado pelo if
+    //Stores the percent
     localStorage.setItem('waterPercent', waterPercent)
-
+    
     //here I transform the waterPercent to string, to update the css properly
+    //I needed the if, because sometimes the percent doesn't stores properly
     let heightUpdate = waterPercent.toString()
     waterInside.style.height = `${heightUpdate}%`
     percentText.textContent = `${heightUpdate}%`
+    percentActual.textContent = `${heightUpdate / 50}L`
+
     
 }
 
@@ -120,7 +119,6 @@ const changeSmallCupStyle = event => {
 }
 
 //listeners
-
 smallCups.forEach(cup => {
     cup.addEventListener('click', updateWater)
 })
